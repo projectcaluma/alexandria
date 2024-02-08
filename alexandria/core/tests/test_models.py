fbac20b5c86bc97ae3f8b74da071f27f3e0612ea
Jonas Metzener <jonas.metzener@adfinis.com>
2024-02-08 18:10:55 +0100

    fix(document): fix clone method on document
    - Actually copy the file on the storage when cloning
    - Generate thumbnails for cloned files

--- alexandria/core/tests/test_models.py
+++ alexandria/core/tests/test_models.py
@@ -1,17 +1,39 @@
 import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-from alexandria.core.models import File
+from alexandria.core.factories import FileData
+from alexandria.core.models import Document, File
 
 
-@pytest.mark.parametrize("admin_groups", [["foo"]])
-def test_clone_document(admin_client, file_factory):
-    v1_thumb = file_factory(variant=File.Variant.THUMBNAIL)
-    v2_thumb = file_factory(variant=File.Variant.THUMBNAIL, document=v1_thumb.document)
-    clone = v1_thumb.document.clone()
+@pytest.mark.parametrize("content_type", ["png", "unsupported"])
+def test_clone_document(db, file_factory, content_type):
+    original_file = file_factory(
+        variant=File.Variant.ORIGINAL,
+        content=SimpleUploadedFile(
+            name="test.png",
+            content=getattr(FileData, content_type),
+            content_type=content_type,
+        ),
+    )
 
-    # in the test, the thumbnail for the cloned file is not created
-    # because the minio hook is not called
-    assert File.objects.count() == 5
-    assert list(clone.files.values_list("name", flat=True)) == [
-        v2_thumb.document.files.filter(variant=File.Variant.ORIGINAL).first().name,
-    ]
+    if content_type != "unsupported":
+        # generate original thumbnail
+        file_factory(
+            original=original_file,
+            variant=File.Variant.THUMBNAIL,
+            document=original_file.document,
+        )
+
+    original_document_pk = original_file.document.pk
+
+    clone = original_file.document.clone()
+    original = Document.objects.get(pk=original_document_pk)
+
+    assert clone.pk != original.pk
+    assert (
+        clone.get_latest_original().content.name
+        != original.get_latest_original().content.name
+    )
+
+    if content_type != "unsupported":
+        assert clone.files.filter(variant=File.Variant.THUMBNAIL).exists()
