projectcaluma/alexandria

commit 39436b7739d8a06c9da2a51f7a7b5bed82a84d4c

fix!: make document title and description not localized
BREAKING CHANGE: This turns the document's `title` and `description`
into regular CharFields/TextFields, instead of localized ones, because
localized fields do not make sense for user-generated data.

Run `./manage.py migrate_document_title_and_description
<source_language>` after deployment to fix existing database records.

alexandria/core/views.py
@@ -136,9 +136,7 @@ class DocumentViewSet(PermissionViewMixin, VisibilityViewMixin, ModelViewSet):
             user=user,
             group=group,
             category=document.category,
-            document_title={
-                k: f"{ splitext(v)[0]}.pdf" for k, v in document.title.items()
-            },
+            document_title=document.title,
             file_name=file_name,
             file_content=ContentFile(response.content, file_name),
             mime_type="application/pdf",

alexandria/conftest.py
@@ -120,8 +120,8 @@ def document_post_data(category):
             b"%PDF-1.\ntrailer<</Root<</Pages<</Kids[<</MediaBox[0 0 3 3]>>]>>>>>>"
         ),
         "data": io.BytesIO(
-            json.dumps(
-                {"title": {"en": "winstonsmith"}, "category": category.pk}
-            ).encode("utf-8")
+            json.dumps({"title": "winstonsmith", "category": category.pk}).encode(
+                "utf-8"
+            )
         ),
     }

alexandria/core/management/commands/migrate_document_title_and_description.py
@@ -1,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from pghstore import loads
+from tqdm import tqdm
+
+from alexandria.core.models import Document
+
+
+class Command(BaseCommand):
+    help = "Migrate document title and description from localized to unlocalized."
+
+    def add_arguments(self, parser):
+        parser.add_argument("source_language")
+
+    def handle(self, *args, **options):
+        source_language = options["source_language"]
+        for document in tqdm(Document.objects.all()):
+            try:
+                document.title = loads(document.title)[source_language]
+                document.description = loads(document.description)[source_language]
+                document.save()
+            except ValueError:
+                pass

alexandria/core/migrations/0018_alter_document_description_alter_document_title.py
@@ -1,0 +1,26 @@
+# Generated by Django 4.2.15 on 2024-08-29 07:51
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("alexandria_core", "0017_file_vector"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="document",
+            name="description",
+            field=models.TextField(
+                blank=True, null=True, verbose_name="document description"
+            ),
+        ),
+        migrations.AlterField(
+            model_name="document",
+            name="title",
+            field=models.CharField(
+                blank=True, null=True, verbose_name="document title"
+            ),
+        ),
+    ]

alexandria/core/models.py
@@ -151,12 +151,8 @@ class Mark(SlugModel):
 
 
 class Document(UUIDModel):
-    title = LocalizedCharField(
-        _("document title"), blank=True, null=True, required=False
-    )
-    description = LocalizedTextField(
-        _("document description"), null=True, blank=True, required=False
-    )
+    title = models.CharField(_("document title"), blank=True, null=True)
+    description = models.TextField(_("document description"), null=True, blank=True)
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,

alexandria/core/tests/test_commands.py
@@ -119,3 +119,17 @@ def test_generate_empty_content_vector(db, settings, file_factory):
 
     assert file_without_vector.content_vector == "'old':1A"
     assert file_without_vector.language is None
+
+
+def test_migrate_document_title_and_description(db, settings, document_factory):
+    doc = document_factory(title='"en"=>"pdf-test.pdf"', description='"en"=>""')
+    doc2 = document_factory(title="already_migrated", description="")
+
+    call_command("migrate_document_title_and_description", "en")
+
+    doc.refresh_from_db()
+    doc2.refresh_from_db()
+    assert doc.title == "pdf-test.pdf"
+    assert doc.description == ""
+    assert doc2.title == "already_migrated"
+    assert doc2.description == ""

alexandria/core/tests/test_filters.py
@@ -74,10 +74,8 @@ def test_document_search_filter(
     """
     tag1 = tag_factory(name="bar_tag")  # matches
     tag2 = tag_factory(name="foo_tag")  # doesn't match
-    doc = document_factory(title={"en": "foo_title", "de": "bar_title"})  # matches
-    doc2 = document_factory(
-        title={"en": "bar_title", "de": "foo_title"}
-    )  # doesn't match
+    doc = document_factory(title="foo_title")  # matches
+    doc2 = document_factory(title="bar_title")  # doesn't match
     doc.tags.add(tag1)
     doc2.tags.add(tag2)
     file_factory(name="filename.pdf", document=doc)
@@ -386,4 +384,4 @@ def test_document_category_filters(
     assert response.status_code == HTTP_200_OK
     data = response.json()["data"]
     assert len(data) == expected_count
-    assert sorted([doc["attributes"]["title"]["en"] for doc in data]) == snapshot
+    assert sorted([doc["attributes"]["title"] for doc in data]) == snapshot

commit c91d7faf947b0e16cc3222b844d782a0944bfdaf

fix(convert): change file extension in document title on conversion
This fixes a regression introduced in 39436b7739d8a06c9da2a51f7a7b5bed82a84d4c

alexandria/core/views.py
@@ -132,11 +132,12 @@ class DocumentViewSet(PermissionViewMixin, VisibilityViewMixin, ModelViewSet):
         )
 
         file_name = f"{splitext(file.name)[0]}.pdf"
+        document_title = f"{splitext(document.title)[0]}.pdf"
         converted_document, __ = create_document_file(
             user=user,
             group=group,
             category=document.category,
-            document_title=document.title,
+            document_title=document_title,
             file_name=file_name,
             file_content=ContentFile(response.content, file_name),
             mime_type="application/pdf",

alexandria/core/tests/test_views.py
@@ -403,7 +403,7 @@ def test_convert_document(
     admin_client, document_factory, file_factory, settings, mocker
 ):
     settings.ALEXANDRIA_ENABLE_PDF_CONVERSION = True
-    document = document_factory()
+    document = document_factory(title="document.docx")
     file_factory(document=document, name="foo.docx")
 
     response = mocker.Mock()
@@ -417,9 +417,14 @@ def test_convert_document(
 
     assert response.status_code == HTTP_201_CREATED
 
-    assert Document.objects.all().count() == 2
-    assert File.objects.all().count() == 4
-    assert File.objects.filter(name="foo.pdf", variant=File.Variant.ORIGINAL).exists()
+    converted_document = Document.objects.get(pk=response.json()["data"]["id"])
+    assert converted_document
+    assert converted_document.title == "document.pdf"
+    assert converted_document.files.count() == 2
+
+    converted_file = converted_document.files.get(variant=File.Variant.ORIGINAL)
+    assert converted_file
+    assert converted_file.name == "foo.pdf"
 
 
 def test_convert_document_not_enabled(