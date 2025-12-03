b0a8bac9ec0f8270638b03fccc4813407ee72555
Martin Huber <martin.huber@karakun.com>
2025-12-03 17:43:38 +0100

    chore: manage scripts improvements

--- alexandria/core/management/commands/generate_missing_thumbnails.py
+++ alexandria/core/management/commands/generate_missing_thumbnails.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import filesizeformat, truncatechars
 from psutil import virtual_memory
 from tqdm import tqdm
 
@@ -10,9 +11,11 @@ class Command(BaseCommand):
     help = "Finds documents that don't have a thumbnail and generates them."
 
     def handle(self, *args, **options):
-        for file in tqdm(
-            File.objects.filter(variant="original", renderings__isnull=True)
-        ):
+        pbar = tqdm(File.objects.filter(variant="original", renderings__isnull=True).order_by("size"))
+        for file in pbar:
+            pbar.set_description(
+                f"Processing {truncatechars(file.name, 50) :<50} ({filesizeformat(file.size) :>10})"
+            )
             create_thumbnail(file.pk)
             if virtual_memory().available < 300_000_000:
                 print("about to run out of memory, stopping")
